4782816f4f13da329503a52eae1438003e1ce796
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-01-21 11:43:27 +0100

    NEW CHANGES

--- admin/upload_result.php
+++ admin/upload_result.php
@@ -19,12 +19,24 @@ if (!isset($_SESSION["admin"])) {
 
 <body>
     <?php include "header_sidebar.php" ?>
-    <div class="main container">
+    <div class="main container-fluid">
         <div class="row">
             <!-- Space Left -->
             <div class="col-md-3"></div>
             <!-- Main Content -->
-            <div class="col-md-9"></div>
+            <div class="col-md-9">
+                <style>
+                    .uploadCard {
+                        border-radius: 14px;
+                        box-shadow: 0 1px 1px rgb(0, 0, 0, 0.2)
+                    }
+                </style>
+                <div class="uploadCard my-5">
+                    <div class="container">
+                        <h1 class="text-start text-primary h3">Upload Results</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
