fbc34f98748b8b7167f96ea932836715cdbda9e1
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-05-25 12:52:25 +0100

    dark mode corrections - AyExpertDev --------- www.ayexpertdev-portfolio.vercel.app

--- notifications.php
+++ notifications.php
@@ -104,6 +104,7 @@ $insertStmt->execute();
             align-items: center;
             justify-content: space-between;
             margin-bottom: 1.5rem;
+            
         }
     </style>
 </head>
